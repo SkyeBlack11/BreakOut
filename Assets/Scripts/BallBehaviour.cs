4dac271ed56f4e860d7d50f335fd79c6ec10a69e
SkyeBlack11 <112585620+SkyeBlack11@users.noreply.github.com>
2024-09-18 12:56:40 -0700

    Single Player Pong
    Functionality of Break Out is working. Next step is to get the tiles to destroy

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -4,33 +4,44 @@ using UnityEngine;
 
 public class BallBahaviour : MonoBehaviour
 {
-    public float VerticalSpeed; //Renamed to make it's purpose clearer
-    private float _maxHorizontalSpeed; //Add new float, TODO:give it a range???
-    public static float Range(0-_maxHorizontalSpeed,_maxHorizontalSpeed); //Generate a random velocity
-    private Vector3 _velocity;
-
-
-    // Start is called before the first frame update
+    public Vector3 Velocity;
+    public float MaxHorizontalVelocity;
+    private Vector3 _position, _scale, _paddlePosition, _paddleScale;
     void Start()
     {
-        _velocity = Vector3.down;
-        _velocity = new Vector3(0, -1, 0);
+
     }
 
-    // Update is called once per frame
-    private static _paddle;
-    public static PaddleSize; //It's angry with the ";" ??
     void Update()
     {
-        _paddle = PaddleController.Instance;
-        PaddleSize = transform.localScale;
 
-        if (transform.position.x + PaddleSize.x
-            >= _paddle.transform.position.x - _paddle.PaddleSize.x / 2f){
-            //Debug.Log("Circle in Rectangle"); //I think the if statement is a bit broken cause this prints when the ball hasn't even moved
-            transform.position += newVector3(_velocity.x, _velocity.y * VerticalSpeed) * Time.deltaTime; //Break up _velocity * Speed
-            _velocity = 0 - _velocity; // set the ball’s vertical velocity to negative its current value TODO: give random horizontal velocity (x)
+        //Debug.Log(PaddleController.Instance.transform.position.x);
+        _position = transform.position;
+        _scale = transform.localScale;
+        _paddlePosition = PaddleController.Instance.transform.position;
+        _paddleScale = PaddleController.Instance.transform.localScale;
+
+        if( //This one is for intersection
+            _position.x + _scale.x /2f >= _paddlePosition.x - _paddleScale.x /2f &&
+            _position.x - _scale.x /2f <= _paddlePosition.x + _paddleScale.x /2f &&
+            _position.y + _scale.y /2f >= _paddlePosition.y - _paddleScale.y /2f &&
+            _position.y - _scale.y /2f <= _paddlePosition.y + _paddleScale.y /2f)
+            {
+                //Debug.Log("Maybe we are intersecting");
+                Velocity.y = -Velocity.y;
+                Velocity.x = Random.Range(-MaxHorizontalVelocity, MaxHorizontalVelocity);
+            }
+        //This one is for bounce and walls
+        if(_position.y > PaddleController.Instance.MaxPosition.transform.position.y)
+            Velocity.y = -Velocity.y;
+        if(_position.x > PaddleController.Instance.MaxPosition.transform.position.x || 
+            _position.x < PaddleController.Instance.MinPosition.transform.position.x)
+        {
+            Velocity.x = -Velocity.x;
         }
-        
+
+        transform.position += Velocity * Time.deltaTime;
     }
+        
 }
+
--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -5,47 +5,30 @@ using UnityEngine;
 public class PaddleController : MonoBehaviour
 {
 
-    public Color PressedColor;
     public Vector3 StartPosition, StartScale;
-    private Color _startColor;
     public float Speed;
-    public Transform MinBounds, MaxBounds; //let’s re-use those handy min and max bounds variables (1.4 walls)
-    //public GameObject MaxPositionX, MinPositionX;
+    public GameObject MinPosition, MaxPosition;
     public static PaddleController Instance;
-    // Start is called before the first frame update
+    
     void Start()
     {
         Instance = this;
-        _startColor = GetComponent<SpriteRenderer>().color;
         transform.position = StartPosition;
         transform.localScale = StartScale;    
     
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
 
-        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x - StartScale.x /2f > MinPositionX.transform.position.x){
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x - StartScale.x /2f > MinPosition.transform.position.x){
             transform.position += Vector3.left * Time.deltaTime * Speed;
-
         }
 
-        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x + StartScale.x /2f < MaxPositionX.transform.position.x){
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x + StartScale.x /2f < MaxPosition.transform.position.x){
             transform.position += Vector3.right * Time.deltaTime * Speed;
-
         }
 
-        if (Input.GetKeyDown(KeyCode.Space)){
-            GetComponent<SpriteRenderer>().color = PressedColor;
-            Debug.Log("Hello");
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space)){
-            GetComponent<SpriteRenderer>().color = _startColor;
-        }
-    
-        //Pseudo Code if(BallPosition >= Left Wall || BallPosition <= Right Wall){ HoriVelocity = 0-HoriVelocity}
-        //if(BallPosition >= MaxBound.y){Velocity = 0-Velocity}
     }
 }
